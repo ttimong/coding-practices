47f20d86e29e33936dcade3b6985063ee80d708b
TimOng <timmy.ong.90@gmail.com>
2017-12-15 21:36:20 +0800

    remove wrong folder

--- sql/hackerrank/the-report.sql
+++
@@ -1,4 +0,0 @@
-select if(g.grade >=8, s.name, "NULL"), g.grade, s.marks
-from students as s inner join grades as g
-on s.marks >= g.min_mark and s.marks <= g.max_mark
-order by g.grade desc, s.name asc, s.marks asc
